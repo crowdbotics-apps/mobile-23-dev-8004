db7916ad78ad253a4729defc9565e8b826cf0f94
Crowdbotics <team@crowdbotics.com>
2020-07-23 08:14:43 +0000

    Update components

--- src/features/BlankScreen39275/screens/blank.js
+++ src/features/BlankScreen39275/screens/blank.js
@@ -27,7 +27,6 @@ export class _Blank extends React.Component {
     Input_5: "",
     Datepicker_6: new Date("07/23/2020"),
     Toggle_7: true,
-    CheckBox_8: true,
     Radio_9: true
   }
 
@@ -68,76 +67,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Button
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "center",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#3366FF",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none",
-          lineHeight: "1"
-        }}
-        onPress={() => alert("Pressed!")}
-      >
-        Press me!
-      </Button>
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none",
-          lineHeight: "1"
-        }}
-      >
-        Sample text content
-      </Text>
       <Input
         placeholder="Sample text input placeholder"
         editable={true}
@@ -282,37 +211,6 @@ export class _Blank extends React.Component {
         }}
         checked={this.state.Toggle_7}
         onChange={nextChecked => this.setState({ Toggle_7: nextChecked })}
-      />
-      <CheckBox
-        text="Checkbox"
-        disabled={false}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none",
-          lineHeight: "1"
-        }}
-        checked={this.state.CheckBox_8}
-        onChange={nextChecked => this.setState({ CheckBox_8: nextChecked })}
       />
       <Radio
         text="Radio button"
