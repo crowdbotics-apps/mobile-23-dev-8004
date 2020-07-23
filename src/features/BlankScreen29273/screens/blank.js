290027f3e0ffb8ca821157bdc4d75d64896a5ae8
Crowdbotics <team@crowdbotics.com>
2020-07-23 16:07:58 +0000

    Update components

--- src/features/BlankScreen29273/screens/blank.js
+++ src/features/BlankScreen29273/screens/blank.js
@@ -103,11 +103,50 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "line-through",
+          textTransform: "lowercase",
+          lineHeight: 1
+        }}
+      >
+        testing
+      </Text>
       <Input
         placeholder="Sample text input placeholder"
         editable={true}
         multiline={true}
-        textStyle={{ height: 100, fontSize: 20, color: "#000000" }}
+        textStyle={{
+          height: 100,
+          fontSize: 20,
+          color: "#000000",
+          textAlign: "left"
+        }}
         style={{
           width: "100%",
           marginLeft: 0,
@@ -175,44 +214,10 @@ export class _Blank extends React.Component {
         checked={this.state.Toggle_4}
         onChange={nextChecked => this.setState({ Toggle_4: nextChecked })}
       />
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "line-through",
-          textTransform: "lowercase",
-          lineHeight: 1
-        }}
-      >
-        testing
-      </Text>
       <Input
         placeholder="Sample text input placeholder"
         editable={true}
-        textStyle={{ fontSize: 20, color: "#000000" }}
+        textStyle={{ fontSize: 20, color: "#000000", textAlign: "left" }}
         style={{
           width: "100%",
           marginLeft: 0,
